ae3b12c4d19f4192c83789965ce011094644b244
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 09:14:23 +0000

    Deploy abdurashidovtaha/context-purchases to github.com/abdurashidovtaha/context-purchases.git:gh-pages

--- components/Loader/Loader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import loader from './45.gif';
-
-export default function Loader() {
-    return (
-        <img src={loader} alt=""/>
-    )
-}
---
+++ precache-manifest.c2f675d40d1fa751193d355bbf695954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e413f22c178e0f95e1930e4517512512",
+    "url": "/context-purchases/index.html"
+  },
+  {
+    "revision": "7d1378e0e16fe26a325c",
+    "url": "/context-purchases/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "3d269bc6b03dbbdb6a0c",
+    "url": "/context-purchases/static/js/2.5a4f8e9c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/context-purchases/static/js/2.5a4f8e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1378e0e16fe26a325c",
+    "url": "/context-purchases/static/js/main.102433c1.chunk.js"
+  },
+  {
+    "revision": "547ba150d233a2b395ee",
+    "url": "/context-purchases/static/js/runtime-main.cfcc24ef.js"
+  },
+  {
+    "revision": "fec456b1f078cac42a59383c4124f466",
+    "url": "/context-purchases/static/media/45.fec456b1.gif"
+  }
+]);
